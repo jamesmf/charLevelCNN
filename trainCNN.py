c0f96358b6754b5f4bd17c983e0eacf0a9881d42
jamesmf <jamesmfrick@gmail.com>
2018-11-13 00:41:44 -0500

    start refactor

--- trainCNN.py
+++ trainCNN.py
@@ -5,95 +5,36 @@ Created on Mon Nov 13 19:56:26 2017
 @author: jmf
 """
 from keras.layers import Input, Dense, Conv1D, Embedding
-from keras.layers import GlobalMaxPooling1D, Dot, Dropout
+from keras.layers import GlobalMaxPooling1D, Dot, Dropout, Flatten
+from keras.regularizers import l1_l2
 from keras.models import Model, load_model
 from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 import numpy as np
 import os
 import pickle
-
-def cos(vector, mat):
-    n1 = np.linalg.norm(vector)
-    n2 = np.linalg.norm(mat, axis=1)
-    dot = mat.dot(vector.T)
-    d = np.divide(dot, n1)
-    try:
-        d = np.divide(d, n2)
-    except ValueError:
-        d = np.divide(d, n2.reshape(-1,1))
-    return d
+from gensim.models import KeyedVectors
 
 
-def readWordVecs(fname="data/ft_100k.txt", style="glove"):
+def read_word_vecs(fname="models/ag_w2v_gensim"):
     """
-    Reads in a flat file of word vectors and returns a numpy array and a list
-    of words. Also creates a list of the letters contained in those words.
-    Currently ignored non-ASCII input. Assumes a GloVe style output, but can
-    be modified to support .vec files.
+    Uses gensim to read in word vectors from a flat file. gensim has scripts
+    to convert between formats, so we can put them all in the w2v format, which
+    can then be loaded with KeyedVectors
     """
-    words = []
-    letters = set()
-    vecLen = 0
-    # iterate over input file to see how many legal words we have
-    with open(fname, "rb") as f:
-        for line in f:
-            try:
-                line = line.decode("UTF-8").encode('ASCII')
-                word = line[:line.find(' ')]
-                words.append(word)
-                if vecLen == 0:
-                    vecLen = len(line.split(' ')) - 2
-                letters = set(letters)|set(word)
-            except UnicodeError:
-                pass
-    n = len(words)
-    print("read in "+str(n)+" words")
-    # define an array of the appropriate size
-    out = np.zeros((n, vecLen))
-    ind = 0
-    with open(fname, "rb") as f:
-        for line in f:
-            try:
-                line = line.decode("ASCII")
-                vec = [np.float16(i) for i in line[line.find(' ')+1:].split()]
-                out[ind,:] = vec
-                ind += 1
-                if divmod(ind, 100000)[1] == 0:
-                    print(ind)
-            except UnicodeError:
-                pass
+    model = KeyedVectors.load_word2vec_format(fname, binary=False)
+    words = sorted(list(model.vocab.keys()))
+    letters = set(['a'])
+    for w in words:
+        for l in w:
+            letters.add(l)
     letters = sorted(letters)
     lettersDict = {k: letters.index(k)+1 for k in letters}
-    return out, words, lettersDict
+    return model, words, lettersDict
 
 
-def getTopN(mat, k=10, chunksize=200):
+def split_inds(words, train=0.8, val=0.1, test=0.1):
     """
-    Function to return a np array of size (vocab, k) corresponding to the k
-    most cosine similar words to the word at index i. If the function has
-    already computed it and stored it in the data/ folder, it loads the file
-    instead.
-    
-    Increasing chunksize speeds up computation at the cost of quadratic
-    increase in memory.
-    """
-    if os.path.isfile("data/topN.npy"):
-        return np.load("data/topN.npy")
-    out = np.zeros((mat.shape[0], k))
-    numChunks = int(mat.shape[0]/chunksize) + (mat.shape[0] % chunksize != 0)*1
-    for i in range(0, numChunks):
-#        if divmod(i, 1000)[1] == 0:
-        print(i, '/', numChunks)
-        c = cos(mat[i*chunksize:(i+1)*chunksize], mat)
-        part = np.argpartition(-c, k, axis=0)[:k].T
-        out[i*chunksize:(i+1)*chunksize, :] = part
-    np.save("data/topN", out)
-    return out
-
-
-def splitInds(words, train=0.8, val=0.1, test=0.1):
-    """
-    Random train/val/test split of the word indices
+    Random train/val/test split of the words
     """
     l = len(words)
     r = np.arange(l)
@@ -103,76 +44,106 @@ def splitInds(words, train=0.8, val=0.1, test=0.1):
     train = r[:trainInd]
     val = r[trainInd:valInd]
     test = r[valInd:]
+    train = [words[i] for i in train]
+    val = [words[i] for i in val]
+    test = [words[i] for i in test]
     return train, val, test
 
 
-def getWordRep(word, letters, maxCharLen):
-    out = np.zeros((1, maxCharLen))
-    maxStart = max(0, maxCharLen - len(word))
+def get_char_level_rep(word, letters, max_char_len):
+    """
+    Given a word, the letter_to_embedding_index mapping, and the maximum
+    sequence length, return a vector representing the word at character-level
+    Args:
+        word (str): word to represent
+        letters (dict): mapping from character -> index in embedding layer
+        max_char_len (int): maximum sequence length in our model
+    """
+    out = np.zeros((1, max_char_len))
+    maxStart = max(0, max_char_len - len(word))
     start = np.random.randint(0, maxStart+1)
     for i in range(0, len(word)):
         if word[i] in letters:
             out[0, start+i] = letters[word[i]]
-        if (start+i+1) == maxCharLen:
+        if (start+i+1) == max_char_len:
             break
     return out
 
 
-def createExamples(mat, topN, indsSplit, indsAll, words, letters,
-                   randToPosRatio, style="vec", maxCharLen=20):
+def create_examples(model, w1_words, w2_words, letters, max_char_len,
+                    sim_examples_per_w=1, rand_examples_per_w=1):
     """
-    Returns a random set of examples to train on. The outputs are (X1, X2, y).
-    If style=="vec" then X1 is the vectors for the sampled words, X2 is the
-    characters in the words associated with the ints in indsSplit, and y is
-    the cosine similarity between the vectors for those two words. In the
-    future, style=="shared" will return X1 and X2 both as the characters in
-    the associated words. 
+    Given a gensim KeyedVectors model, return ((w1, w2), cos(vw1, vw2)) pairs
+    where w1 and w2 are character-level representations of words. Words are
+    chosen from model.words[inds]. For each w1, we sample w2's in some ratio
+    of similar:random expressed by sim_ and rand_examples_per_w. We do this
+    using model.most_similar(w1) and sampling from the result for (similar) and
+    sampling randomly for (random).
     
-    indsSplit : array of the indices of the set you're in (train/val/test)
-
-    indsAll : array of all the indices from which you can draw comparisons
-
-    randToPosRatio: ratio at which to draw 'negative' random samples for each
-                    positive sample (from the top-k most similar words)
+    When training we want w1_inds == w2_inds == training_inds. For dev we want
+    w1_inds = dev_inds, w2_inds = [training+dev] and for test we want
+    w1_inds = test_inds, w2_inds = [training+dev+test]. That way we are
+    evaluating entirely unseen examples, but not restricting 
+    Args:
+        model (gensim.models.KeyedVectors): vector model
+        w1_words (list): list of words in current set (train/dev/test)
+        w2_words (list): all words the model has seen before
+        letters (dict): mapping from character -> index in embedding layer
+        max_char_len (int): max length of a word in characters
+        sim_examples_per_w (int): number of similar examples per word in set_inds
+        rand_examples_per_w (int): number of random examples per word in set_inds
+    Returns:
+        [[X1, X2], [y]]: X's are char-level reps, y is cosine_similarity
     """
-    # samples is length len(indsSplit)*(1+randToPosRatio)
-    randToPosRatio = int(randToPosRatio)
-    outSize = len(indsSplit)*(1+randToPosRatio)
-    X1 = np.zeros((outSize, mat.shape[1]))
-    X2 = np.zeros((outSize, maxCharLen), dtype=np.uint8)
-    y = np.zeros(outSize)
-    outIndex = 0
-    for wordInd in indsSplit:
-        posInd = int(np.random.choice(topN[wordInd]))
-        vec1 = mat[posInd]
-        vec2 = mat[wordInd]
-        c = cos(vec1, vec2.reshape(-1,1).T)
-        wordRep = getWordRep(words[wordInd], letters, maxCharLen)
-        X1[outIndex] = vec1
-        X2[outIndex] = wordRep
+    # get the words our model can see
+    w2_set = set(w1_words+w2_words)
+
+    # vector size
+    vec_size = max_char_len
+    
+    # how many examples per word
+    ex_per = sim_examples_per_w + rand_examples_per_w
+
+    # initialize some big X1, X2, y matrices
+    X1 = np.zeros((len(w1_words)*(ex_per), vec_size))
+    X2 = np.zeros((len(w1_words)*(ex_per), vec_size))
+    y = np.zeros((len(w1_words)*ex_per, 1))
+
+    # iterate over them, inserting examples into X1, X2, y
+    for word_ind, w in enumerate(w1_words):
+        # for all the next samples, X1 should have the vector for w
+        offset = ex_per * word_ind
+        rep_x1 = get_char_level_rep(w, letters, max_char_len)
+        X1[offset:offset+ex_per] = rep_x1
+
+        # sample some similar words
+        most_sim = model.most_similar(w, topn=100*ex_per)
+        most_sim = [i for i in most_sim if i[0] in w2_set]
+
+        sim_range = np.arange(0, len(most_sim))
+        sim_w = np.random.choice(sim_range, size=sim_examples_per_w)
+        sim_w = [most_sim[ind] for ind in sim_w]
+
+        # put our sampled similar words into X2
+        sim_reps_x2 = [get_char_level_rep(sw[0], letters, max_char_len) for sw in sim_w]
+        X2[offset:offset+sim_examples_per_w] = sim_reps_x2
+        y[offset:offset+sim_examples_per_w] = [i[1] for i in sim_w]
+
+        # get some random words
+        rand_words = np.random.choice(w2_words, size=rand_examples_per_w)
+        rand_reps_x2 = [get_char_level_rep(rw, letters, max_char_len) for rw in rand_words]
+        rand_sims = [model.similarity(w, r) for r in rand_words]
+        y[offset+sim_examples_per_w:offset+ex_per] = [i for i in rand_sims]
+        X2[offset+sim_examples_per_w:offset+ex_per] = rand_reps_x2
+    X = [X1, X2]
+    return X, y
         
-#        y[outIndex] = c
-        y[outIndex] = 1
-        outIndex += 1
-        negInds = [int(i) for i in np.random.choice(indsAll,
-                                                    size=randToPosRatio)]
-        vecNeg = mat[negInds]
-#        print(words[wordInd], [words[int(i)] for i in negInds])
-        cRand = cos(vec2, vecNeg)
-#        print(cRand.shape, cRand)
-        X1[outIndex:outIndex+randToPosRatio] = vecNeg
-        for j in range(0, randToPosRatio):
-            X2[outIndex+j] = getWordRep(words[wordInd], letters, maxCharLen)
-#        y[outIndex: outIndex+randToPosRatio] = cRand
-        outIndex += randToPosRatio
 
-    return X1, X2, y
-
-
-def exampleGen(*args, **kwargs):
-    batchSize = 32
+def example_gen(*args, **kwargs):
+    batchSize = 8
     while True:
-        X1, X2, y = createExamples(*args, **kwargs)
+        X, y = create_examples(*args, **kwargs)
+        X1, X2 = X
         inds = np.arange(0, X1.shape[0])
         np.random.shuffle(inds)
         ind = 0
@@ -183,76 +154,73 @@ def exampleGen(*args, **kwargs):
             ind += batchSize
 
 
-def checkScore(word, chars, letters, words, maxCharLen):
-    numToCheck = 20
-    x1 = []
-    x2 = []
-    for i in range(0, numToCheck):
-        x1.append(mat[words.index(word)])
-        x2.append(getWordRep(chars, letters, maxCharLen))
-    x1 = np.array(x1).reshape(numToCheck,-1)
-    x2 = np.array(x2).reshape(numToCheck,-1)
-    p = model.predict([x1, x2])
-    print(np.mean(p))
+def get_model_layers(inps, embedding_size):
+    layers = []
+    layers.append(Embedding(len(letters)+1, embedding_size))
+    layers.append(Conv1D(64, 3, dilation_rate=1, activation='relu',
+                         activity_regularizer=l1_l2(0.05, 0.05)))
+    layers.append(Conv1D(128, 3, dilation_rate=2, activation='relu',
+                         activity_regularizer=l1_l2(0.05, 0.05)))
+    layers.append(Dropout(0.1))
+    layers.append(Conv1D(128, 3, dilation_rate=1, activation='relu',
+                         activity_regularizer=l1_l2(0.05, 0.05)))
+    layers.append(Conv1D(128, 3, dilation_rate=2, activation='relu',
+                         activity_regularizer=l1_l2(0.05, 0.05)))
+    layers.append(Dropout(0.1))
+
+    for layer in layers:
+        for n, side in enumerate(inps):
+            inps[n] = layer(side)
+    return inps
 
 
-def defineModel(letters, maxCharLen, vecLen):
-    sharedSize = 128
-    charInp = Input(shape=(maxCharLen,))
-    vecInp = Input(shape=(vecLen,))
-    
-    # character embedding
-    char = Embedding(len(letters)+1, 10)(charInp)
-    char = Conv1D(32, 3, padding="same", dilation_rate=1,
-                  activation='relu')(char)
-    char = Conv1D(64, 3, padding="same", dilation_rate=2,
-                  activation='relu')(char)
-    char = Conv1D(sharedSize, 3, padding="same", dilation_rate=4,
-                  activation='relu')(char)
-    char = Conv1D(sharedSize, 3, padding="same", dilation_rate=8,
-                  activation='relu')(char)
-    char = GlobalMaxPooling1D()(char)
-    char = Dropout(0.5)(char)
+def define_model(letters, max_char_len):
+    shared_size = 128
+    char_inp_left = Input(shape=(max_char_len,))
+    char_inp_right = Input(shape=(max_char_len,))
+    inps = [char_inp_left, char_inp_right]
+    left, right = get_model_layers(inps, 64)
 
-    # vector representation
-    d = Dense(sharedSize, activation='tanh')(vecInp)
+    # share a dense layer to project from flatten to something to be dotted
+    shared_dense = Dense(shared_size, activation='tanh')
+    left = Flatten()(left)
+    left = shared_dense(left)
+
+    right = Flatten()(right)
+    right = shared_dense(right)
     
     # merge
-    dot = Dot(1, normalize=True)([char, d])
-    dot = Dense(1, activation='sigmoid')(dot)
+    dot = Dot(1, normalize=True)([left, right])
     
-    model = Model([vecInp, charInp], dot)
-    model.compile('adam', 'binary_crossentropy')
+    model = Model([char_inp_left, char_inp_right], dot)
+    model.compile('adam', 'mse')
     return model
 
-k = 10 #  top k most similar words are considered "positives" in ratio below
-randToPosRatio = 4 #  for every word taken from top-k, this many random samples
-maxCharLen = 20 #  maximum word length we'll allow
 
-mat, words, letters = readWordVecs()
-topN = getTopN(mat, k=k)
-train, val, test = splitInds(words)
+max_char_len = 20 #  maximum word length we'll allow
 
-stepsPer = len(train)*(1+randToPosRatio)/32
-allInds = np.arange(0, len(words))
-X1val, X2val, yval = createExamples(mat, topN, val, allInds, words,
-                                    letters, randToPosRatio)
-X1test, X2test, ytest = createExamples(mat, topN, test, allInds,
-                                    words, letters, randToPosRatio)
+word_model, words, letters = read_word_vecs()
+
+train, val, test = split_inds(words)
+
+steps_per = len(train)/16
+w2_words = train+val
+Xval, yval = create_examples(word_model, val, w2_words, letters, max_char_len)
+w2_words += test
+Xtest, ytest = create_examples(word_model, test, w2_words, letters, max_char_len)
 
 callbacks = [
-    EarlyStopping(patience=8),
+    EarlyStopping(patience=16),
     ModelCheckpoint(filepath='models/charLevel.cnn', verbose=1,
                     save_best_only=True),
     TensorBoard() #  not all of the options work w/ TB+keras
 ]
 
-model = defineModel(letters, maxCharLen, X1val.shape[1])
-model.fit_generator(exampleGen(mat, topN, train, allInds, words,
-                                   letters, randToPosRatio),
-                    steps_per_epoch=stepsPer,
+model = define_model(letters, max_char_len)
+model.fit_generator(example_gen(word_model, train, train, letters, max_char_len),
+                    steps_per_epoch=steps_per,
                     epochs=100,
                     callbacks=callbacks,
-                    validation_data=([X1val, X2val],[yval]))
+                    validation_data=(Xval, [yval]))
 with open("models/letters.pkl", 'wb') as f:
     pickle.dump(letters, f)
